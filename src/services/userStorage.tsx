ba49c1fc32444049ed9dec1ee4265d08a200211c
Zennisch <snowy.sunny.dazing.us@gmail.com>
2025-04-09 07:35:01 +0900

    update: docstring

--- src/services/userStorage.tsx
+++ src/services/userStorage.tsx
@@ -1,22 +1,38 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {User} from '@/src/models/User';
 
+
+// Một hằng số lưu trữ khóa. Được sử dụng để lưu trữ thông tin người dùng trong bộ nhớ.
 const USER_STORAGE_KEY = '@IUH_CNM_APP:user';
 
 export const storage = {
-    // Lưu thông tin user
+
+    /*
+    * Lưu thông tin người dùng vào bộ nhớ
+    *
+    * Được sử dụng cho chức năng đăng nhập
+    *
+    * - Nhận đối tượng User làm tham số
+    * - Loại bỏ thuộc tính password khỏi đối tượng trước khi lưu nhằm mục đích bảo mật
+    * - Chuyển đổi đối tượng thành chuỗi JSON
+    * - Lưu chuỗi JSON vào bộ nhớ bằng khóa USER_STORAGE_KEY
+     */
     async saveUser(user: User): Promise<void> {
         try {
-            // Loại bỏ password trước khi lưu
             const {password, ...userToStore} = user;
-
             await AsyncStorage.setItem(USER_STORAGE_KEY, JSON.stringify(userToStore));
         } catch (error) {
             console.error('Error saving user to storage:', error);
         }
     },
 
-    // Lấy thông tin user
+    /*
+    * Lấy thông tin người dùng từ bộ nhớ
+    *
+    * Được sử dụng cho chức năng xác thực người dùng, kiểm tra trạng thái đăng nhập,...
+    *
+    * - Lấy dữ liệu từ bộ nhớ bằng khóa USER_STORAGE_KEY
+     */
     async getUser(): Promise<Partial<User> | null> {
         try {
             const userJson = await AsyncStorage.getItem(USER_STORAGE_KEY);
@@ -27,7 +43,13 @@ export const storage = {
         }
     },
 
-    // Xóa thông tin user (đăng xuất)
+    /*
+    * Xóa thông tin người dùng khỏi bộ nhớ
+    *
+    * Được sử dụng cho chức năng đăng xuất
+    *
+    * - Xóa dữ liệu khỏi bộ nhớ bằng khóa USER_STORAGE_KEY
+     */
     async removeUser(): Promise<void> {
         try {
             await AsyncStorage.removeItem(USER_STORAGE_KEY);
