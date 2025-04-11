eb6d21599f86e16f36126d1d6ca62e7a50e5f604
Zennisch <snowy.sunny.dazing.us@gmail.com>
2025-04-12 03:37:23 +0900

    update: use user

--- src/hooks/UseUser.ts
+++ src/hooks/UseUser.ts
@@ -1,24 +1,14 @@
 import {useAuth} from '@/src/contexts/UserContext';
 import {Profile} from '@/src/models/Profile';
+import {User} from "@/src/models/User";
 
 export function UseUser() {
     const {user} = useAuth();
 
-    // Kiểm tra xem người dùng đã được xác thực hay chưa
     const isAuthenticated = !!user;
 
-    // Nếu người dùng đã được xác thực, tạo một đối tượng profile từ thông tin của người dùng
-    const profile = user ? ({
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        phone: user.phone,
-        gender: user.gender,
-        avatarURL: user.avatarURL,
-        coverURL: user.coverURL,
-        dob: user.dob,
-        isOnline: user.isOnline,
-    } as Profile) : null;
+    const profile = user ? (({password, createdAt, updatedAt, ...profileData}) =>
+        profileData as Profile)(user as User) : null;
 
     return {user, isAuthenticated, profile};
 }
