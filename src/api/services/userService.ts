b52070cf0a7db6d3e601a9021f0b63dfa09ee2c7
Zennisch <snowy.sunny.dazing.us@gmail.com>
2025-04-11 21:54:32 +0900

    update: userService.ts

--- src/api/services/userService.ts
+++ src/api/services/userService.ts
@@ -4,31 +4,6 @@ import { authStorage } from '@/src/services/authStorage';
 import { API_DOMAIN } from '@/src/constants/ApiConstant';
 
 export const userService = {
-    // async getUserByPhone(phone: string): Promise<User | null> {
-    //     try {
-    //         const response = await axios.get(`${ENDPOINT}/User`);
-    //         const matchedUser = response.data.find((user: User) => user.phone === phone);
-    //         return matchedUser || null;
-    //     } catch (error) {
-    //         console.error('Error fetching user by phone:', error);
-    //         return null;
-    //     }
-    // },
-    //
-    // async login(phone: string, password: string): Promise<{ success: boolean; user?: User; message?: string }> {
-    //     try {
-    //         const user = await this.getUserByPhone(phone);
-    //
-    //         if (!user) return {success: false, message: 'Số điện thoại không tồn tại'};
-    //         if (user.password !== password) return {success: false, message: 'Mật khẩu không chính xác'};
-    //
-    //         return {success: true, user: user};
-    //     } catch (error) {
-    //         console.error('Login error:', error);
-    //         return {success: false, message: 'Có lỗi xảy ra, vui lòng thử lại sau'};
-    //     }
-    // },
-
     async update(userData: Partial<User>): Promise<{ success: boolean; user?: User; message?: string; data?: any }> {
         try {
             console.log('Updating user with data:', userData);
