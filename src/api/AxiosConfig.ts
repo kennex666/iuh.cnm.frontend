2a43aede6dc295f1736d04eaba122785780fcfc9
Zennisch <snowy.sunny.dazing.us@gmail.com>
2025-04-15 22:18:35 +0900

    update: axios config

--- src/api/AxiosConfig.ts
+++ src/api/AxiosConfig.ts
@@ -4,11 +4,11 @@ import {Platform} from 'react-native';
 
 const getApiUrl = () => {
     if (Platform.OS === 'web') {
-        return 'http://172.20.77.2:8087/api';
+        return 'http://localhost:8087/api';
     } else if (Platform.OS === 'android') {
-        return 'http://172.20.77.2:8087/api';
+        return 'http://10.0.2.2:8087/api';
     } else {
-        return 'http://172.20.77.2:8087/api';
+        return 'http://localhost:8087/api';
     }
 };
 
